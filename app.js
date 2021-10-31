d16998c1fb8969dcf08fcbcc8c636184d41c5e39
Sandra Muraca <sandramuraca78@gmail.com>
2021-10-31 20:24:08 -0300

    funcionalidades boton agregar nueva operacion y cancelar

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
-//MOSTRAR OCULTAR FILTROS - MENU IZQUIERDO
 const mostrarOcultarFiltros = document.querySelector("#mostrar-ocultar-filtros")
 const formularioFiltros = document.querySelector("#formulario-filtros")
 
+const main = document.querySelector("#main")
+const sectionCardNuevaOperacion = document.querySelector("#section-card-nueva-operacion")
+const cardNuevaOperacion = document.querySelector("#card-nueva-operacion")
+const botonAgregarOperacion = document.querySelector("#boton-agregar-operacion")
+const botonCancelarAgregarOperacion = document.querySelector("#cancelar-agregar-operacion")
+
+//MOSTRAR OCULTAR FILTROS - MENU IZQUIERDO
 mostrarOcultarFiltros.addEventListener("click", ()=>{
     formularioFiltros.classList.toggle("is-hidden")
     
     if (mostrarOcultarFiltros.innerText == 'Ocultar Filtros'){
-        mostrarOcultarFiltros.innerText = 'Mostrar Filtros';
-         } else {
-            mostrarOcultarFiltros.innerText == 'Ocultar filtros';
-         }
-     }
+        mostrarOcultarFiltros.innerText = 'Mostrar Filtros'
+    } else {
+        mostrarOcultarFiltros.innerText = 'Mostrar Filtros'
+    }
+    
+    }
  )
+
+ //MOSTRAR OCULTAR CARD NUEVA OPERACION
+ botonAgregarOperacion.onclick=()=>{
+   sectionCardNuevaOperacion.classList.remove("is-hidden")
+    main.classList.add("is-hidden")
+ }
+
+ botonCancelarAgregarOperacion.onclick=()=>{
+    sectionCardNuevaOperacion.classList.add("is-hidden")
+    main.classList.remove("is-hidden")
+ }
