e3d0274ebab3a29045d843e89dd6afdac5407a31
Sandra Muraca <sandramuraca78@gmail.com>
2021-11-27 17:01:16 -0300

    agrego funcion filtro por fecha

--- app.js
+++ app.js
@@ -339,9 +339,9 @@ mostrarOperacionesEnHTML();
 
 //FILTRO POR TIPO - CATEGORIA - FECHA - ORDEN
 //select por tipo en filtros
-const operacionesFiltradas = [...operaciones]
 
 selectTipoFiltros.onchange = () => {
+  const operacionesFiltradas = [...operaciones]
   const filtradoPorTipo = operacionesFiltradas.filter((operacion) =>{
     if (selectTipoFiltros.value === "Todos"){
       return operacion
@@ -382,7 +382,7 @@ selectTipoFiltros.onchange = () => {
         `
     );
   }, ``);
-
+  
   contenedorGrillaOperaciones.innerHTML = estructuraHTML;
 
   botonesEliminarOperacion();
@@ -395,17 +395,74 @@ mostrarOperacionesFiltradasEnHTML()
 
 //select de categorias en filtros
 selectCategoriasFiltros.onchange = () => {
- 
-  const filtradoPorCategoria = operacionesFiltradas.filter((operacion) =>{
+    const operacionesFiltradas = [...operaciones]
+    const filtradoPorCategoria = operacionesFiltradas.filter((operacion) =>{
+      
     if (selectCategoriasFiltros.value === "Todos"){
       return operacion
     }  
     return operacion.categoria === selectCategoriasFiltros.value
+    
   })
 
 const mostrarOperacionesFiltradasEnHTML = () => {
     operaciones = recuperarDatosDeLS();
     const estructuraHTML = filtradoPorCategoria.reduce((acc, elemento, index) => {
+    return (
+      acc +
+      `
+          <div class="columns is-align-items-center">
+            <div class="column is-3 has-text-weight-semibold">   
+              <p>${elemento.descripcion}</p>
+            </div>
+            
+            <div class="column is-3 ">   
+              <p class="tag">${elemento.categoria}<p>
+            </div>
+  
+            <div class="column is-2 has-text-right">
+              <p class="has-text-grey">${elemento.fecha}</p>
+            </div>
+  
+            <div class="column is-2 has-text-right is-align-items-center ${colorDeMontoOperaciones(elemento.tipo)} has-text-weight-bold">
+              <p>$${elemento.monto}</p>
+            </div>
+               
+            <div class="column is-2 has-text-right">   
+              <p>
+                <button class="button is-ghost is-small boton-editar-operacion" id="boton-editar-${index}">Editar</button>
+                <button class="button is-ghost is-small boton-eliminar-operacion" id="boton-eliminar-${index}">Eliminar</button>
+              </p>
+            </div>
+          </div>    
+        `
+    );
+  }, ``);
+
+  contenedorGrillaOperaciones.innerHTML = estructuraHTML;
+
+  botonesEliminarOperacion();
+  botonesEditarOperacion();
+};
+
+mostrarOperacionesFiltradasEnHTML()
+}
+
+//FILTRO FECHA
+const inputFecha = document.querySelector("#input-fecha")
+
+inputFecha.oninput = () => {
+  
+  const operacionesFiltradas = [...operaciones]
+  const fechaDesde = inputFecha.value
+ 
+  const filtroPorFecha = operacionesFiltradas.filter((operacion) =>{
+    return operacion.fecha > fechaDesde
+  })
+
+  const mostrarOperacionesFiltradasEnHTML = () => {
+    operaciones = recuperarDatosDeLS();
+    const estructuraHTML = filtroPorFecha.reduce((acc, elemento, index) => {
     return (
       acc +
       `
